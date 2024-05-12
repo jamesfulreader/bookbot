a4323c55e03f8b112e96537e7170163ad8c3f612
jamesfulreader <jamesfulreader@gmail.com>
2024-05-12 13:01:14 -0600

    refactor main print text to the console

--- main.py
+++ main.py
@@ -1,5 +1,10 @@
 def main():
-  with open("./books/frankenstein.text") as f:
-    file_contents = f.read()
+  book_path = "books/frankenstein.txt"
+  text = get_book_text(book_path)
+  print(text)
+
+def get_book_text(path):
+  with open(path) as f:
+    return f.read()
 
 main()
