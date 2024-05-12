6c150fea0c15b303e09fe3f94c381c949abe4bbe
jamesfulreader <jamesfulreader@gmail.com>
2024-05-12 13:16:14 -0600

    create count_words func print number of words to console

--- main.py
+++ main.py
@@ -2,9 +2,15 @@ def main():
   book_path = "books/frankenstein.txt"
   text = get_book_text(book_path)
   print(text)
+  num_words = count_words(text)
+  print(f"\nThe number of words contained in the text is: {num_words}")
 
 def get_book_text(path):
   with open(path) as f:
     return f.read()
+
+def count_words(text):
+  words = text.split()
+  return len(words)
 
 main()
